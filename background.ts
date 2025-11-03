97cb273849b89b3cfd16a6a86f40d447ba93e6c5
escape0707 <tothesong@gmail.com>
2025-11-03 17:10:47 +0900

    Initialize as a TypeScript project

--- background.ts
+++ background.ts
@@ -1,4 +1,5 @@
 const menuItemId = "copy-text-fragment-link";
+
 browser.runtime.onInstalled.addListener(() => {
     browser.menus.create({
         id: menuItemId,
@@ -6,18 +7,17 @@ browser.runtime.onInstalled.addListener(() => {
         contexts: ["selection"]
     });
 });
+
 browser.menus.onClicked.addListener(async (info) => {
     if (info.menuItemId === menuItemId) {
-        const url = new URL(info.pageUrl);
-        const encodedText = encodeURIComponent(info.selectionText).replace(/-/g, "%2D");
+        const url = new URL(info.pageUrl!);
+        const encodedText = encodeURIComponent(info.selectionText!).replace(/-/g, "%2D");
         url.hash = `:~:text=${encodedText}`;
         try {
             await navigator.clipboard.writeText(url.toString());
             console.log("Text fragment link copied to clipboard.");
-        }
-        catch (error) {
+        } catch (error) {
             console.error("Failed to copy text fragment link:", error);
         }
     }
 });
-export {};
